a5385ae76c0a6ea09a827d7ad03778f6385020b9
PassionPenguin <41184947+PassionPenguin@users.noreply.github.com>
2020-03-30 20:03:14 +0800

    bind package => build.package.js

--- dist/libs/build.package.js
+++ dist/libs/build.package.js
@@ -38,7 +38,7 @@ const builder = {
 
         builder.importStyles(["./dist/libs/bundle/PenguinUI/bundle.PenguinUI.css", "./dist/libs/bundle/storyboard/bundle.storyboard.css"]);
         ProgressManager.update(0, 33.3, 100);
-        builder.importScripts(["./dist/libs/attachWindowCursorEvent.js", "./dist/libs/initInterface.js", "./dist/libs/lineEditorComp.js", "./dist/libs/exportDrawable.js", "./dist/libs/drawMap.js", "./assets/locales/" + builder.locales[pg.language] + ".js"], returnFunc);
+        builder.importScripts(["./dist/libs/attachWindowCursorEvent.js", "./dist/libs/initInterface.js", "./dist/libs/lineEditorComp.js", "./dist/libs/exportDrawable.js", "./dist/libs/drawMap.js", "./dist/libs/selectLineDialog.js", "./assets/locales/" + builder.locales[pg.language] + ".js"], returnFunc);
         ProgressManager.update(0, 66.6, 100);
     },
     importScripts: (urls, returnFunc) => {
