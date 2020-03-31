c66c244a9572718b6ca9d72b82cfda88bef902b1
PassionPenguin <41184947+PassionPenguin@users.noreply.github.com>
2020-03-31 16:06:13 +0800

    fix media query and add media query support

--- dist/libs/bundle/PenguinUI/require/MediaQuery.js
+++ dist/libs/bundle/PenguinUI/require/MediaQuery.js
@@ -10,6 +10,6 @@
 
 const MediaQuery = {
     screenWidth: () => {
-        return window.innerWidth > 320 ? window.innerWidth > 480 ? window.innerWidth > 640 ? window.innerWidth > 720 ? window.innerWidth > 768 ? window.innerWidth > 1024 ? window.innerWidth > 1920 : "xxlg" : "xlg" : "lg" : "md" : "sm" : "ssm";
+        return window.innerWidth > 320 ? window.innerWidth > 480 ? window.innerWidth > 640 ? window.innerWidth > 720 ? window.innerWidth > 768 ? window.innerWidth > 1024 ? window.innerWidth > 1680 ? "xxlg" : "xlg" : "lg" : "r" : "md" : "sm" : "xs" : "xxs";
     }
 };
