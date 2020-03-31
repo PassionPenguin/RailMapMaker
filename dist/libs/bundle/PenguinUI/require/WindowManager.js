08adf08d56c494290bab2e02ab3e4b6041a1ac90
PassionPenguin <41184947+PassionPenguin@users.noreply.github.com>
2020-03-31 09:03:35 +0800

    separate WindowManager's remove method

--- dist/libs/bundle/PenguinUI/require/WindowManager.js
+++ dist/libs/bundle/PenguinUI/require/WindowManager.js
@@ -9,24 +9,21 @@
  */
 
 const WindowManager = {
+    query: [],
     create: (returnFunc, opt) => {
         opt = opt || {};
         opt.size = opt.size || "middle";
         opt.zIndex = opt.zIndex || "1000";
+        opt.channelId = opt.channelId || WindowManager.query.length;
         let WindowFrame = cE({
             type: "div",
-            attr: [["class", `pg-window ${opt.size}`], ["style", `z-index: ${opt.zIndex}`]]
+            attr: [["class", `pg-window ${opt.size}`], ["style", `z-index: ${opt.zIndex}`], ["windowId", opt.channelId]]
         });
         let WindowMask = cE({
             type: "div",
-            attr: [["class", "pg-window pg-window-mask"], ["style", `z-index: ${Int(opt.zIndex) - 1}`]],
+            attr: [["class", "pg-window pg-window-mask"], ["style", `z-index: ${Int(opt.zIndex) - 1}`], ["maskId", opt.channelId]],
             onclick: () => {
-                WindowFrame.classList.add("remove");
-                setTimeout(() => {
-                    document.body.removeChild(WindowFrame);
-                    document.body.removeChild(WindowMask);
-                    pg.$("#pg-app")[0].style.filter = "";
-                }, 500);
+                WindowManager.remove(opt.channelId);
             }
         });
         let content = cE({type: "div", attr: [["class", "pg-window-content"]]});
@@ -37,14 +34,16 @@ const WindowManager = {
         content.appendChild(cE({
             type: "span", attr: [["class", "pg-window-close mi"]], innerText: "close",
             onclick: () => {
-                WindowFrame.classList.add("remove");
-                setTimeout(() => {
-                    document.body.removeChild(WindowFrame);
-                    document.body.removeChild(WindowMask);
-                    pg.$("#pg-app")[0].style.filter = "";
-                }, 500);
+                WindowManager.remove(opt.channelId);
             }
         }));
-        returnFunc(content);
+        returnFunc(content, opt.channelId);
+    }, remove: (channelId) => {
+        pg.$(`[windowId='${channelId}']`)[0].classList.add("remove");
+        setTimeout(() => {
+            document.body.removeChild(pg.$(`[windowId='${channelId}']`)[0]);
+            document.body.removeChild(pg.$(`[maskId='${channelId}']`)[0]);
+            pg.$("#pg-app")[0].style.filter = "";
+        }, 500);
     }
 };
