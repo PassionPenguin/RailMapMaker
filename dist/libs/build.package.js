3ae4967e27fbaa90002e9d5eb27ed7e72aa2ff65
PassionPenguin <41184947+PassionPenguin@users.noreply.github.com>
2020-03-31 16:06:24 +0800

    upd version

--- dist/libs/build.package.js
+++ dist/libs/build.package.js
@@ -10,9 +10,9 @@
 
 const builder = {
     debugMode: true,
-    version: "1.2",
-    bundles: [{name: "PenguinUI", version: "0.1.4"}],
-    displayVersion: "Cola/(RMG v1.2 Stable)",
+    version: "1.3",
+    bundles: [{name: "PenguinUI", version: "0.1.5"}],
+    displayVersion: "Cola/(RMG v1.3 Stable)",
     locales: ["zh_CN", "zh_HK", "zh_YUE", "en_US"],
     buildTime: 1585118965574,
     debug: (pkg, src, tag, msg) => {
