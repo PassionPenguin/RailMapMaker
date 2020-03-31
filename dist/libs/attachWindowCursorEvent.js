ddd9965ee5b6adccb84257ee76b41269dc733a3a
PassionPenguin <penguin.zhang@qq.com>
2020-03-31 18:22:14 +0800

    fixed incorrect newPath-gen-func

--- dist/libs/attachWindowCursorEvent.js
+++ dist/libs/attachWindowCursorEvent.js
@@ -24,29 +24,28 @@ const attachEvent = {
             }
 
             if (state.newPath) {
-                contentData.push({
-                    name: "Unnamed Project", author: "@User", lastModified: new Date().getTime(), pathInf: {
-                        "lineCap": "round",
-                        "lineJoin": "round",
-                        "strokeWidth": "5px",
-                        "color": "#000",
-                        "id": state.pathId,
-                        "opacity": 1,
-                        "stations": [{"x": curX, "y": curY, "type": "destination", "routeToNext": ""}]
-                    }
+                contentData.pathInfo.push({
+                    "lineCap": "round",
+                    "lineJoin": "round",
+                    "strokeWidth": "5px",
+                    "color": "#000",
+                    "id": state.pathId,
+                    "opacity": 1,
+                    "stations": [{"x": curX, "y": curY, "type": "destination", "routeToNext": ""}]
                 });
                 state.newPath = false;
                 initDrawable();
                 builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Path", "New Path Created: id=" + state.pathId);
             } else {
                 builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Path", "New Station Created: [x,y]=" + curX + ", " + curY);
+                contentData.pathInfo[state.pathId].stations.last().type = "common";
                 contentData.pathInfo[state.pathId].stations.push({
                     x: curX, y: curY, type: "destination", routeToNext: "0"
                 });
                 drawMap(state.pathId);
             }
         },
-        MoveEventListener: (element) => {
+        MoveEventListener: (element, event) => {
             element.setAttribute("style", "left:" + (min(event.offsetX) - pg.$("#pg-app")[0].scrollLeft) + "px; top:" + (min(event.offsetY) - pg.$("#pg-app")[0].scrollTop) + "px;");
             event.stopPropagation();
         },
@@ -54,25 +53,16 @@ const attachEvent = {
         attachClickEvent: (element) => {
             builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Event", "Event Attached: EditStation.Click");
             element.addEventListener("click", attachEvent.EditStation.ClickEventListener);
-        },
-
-        detachClickEvent: (element) => {
-            builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Event", "Event Detached: EditStation.Click");
-            element.removeEventListener("click", attachEvent.EditStation.ClickEventListener);
+            element.onclick = (event) => {
+                attachEvent.EditStation.ClickEventListener(event);
+            }
         },
 
         attachMoveEvent: (element, target) => {
             builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Event", "Event Attached: EditStation.Move");
-            element.addEventListener("mousemove", () => {
-                attachEvent.EditStation.MoveEventListener(target)
+            element.addEventListener("mousemove", (event) => {
+                attachEvent.EditStation.MoveEventListener(target, event)
             });
         },
-
-        detachMoveEvent: (element, target) => {
-            builder.debug("PassionPenguin/RailMapMaker", "attachWindowCursorEvent.js", "Event", "Event Detached: EditStation.Move");
-            element.removeEventListener("mousemove", (target) => {
-                attachEvent.EditStation.MoveEventListener(target)
-            });
-        }
     }
 }
