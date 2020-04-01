219fd008010d82f241fe8be62cfafba3844f91d2
PassionPenguin <penguin.zhang@qq.com>
2020-04-01 11:21:37 +0800

    update MediaQuery

--- dist/libs/bundle/PenguinUI/require/MediaQuery.js
+++ dist/libs/bundle/PenguinUI/require/MediaQuery.js
@@ -10,6 +10,41 @@
 
 const MediaQuery = {
     screenWidth: () => {
-        return window.innerWidth > 320 ? window.innerWidth > 480 ? window.innerWidth > 640 ? window.innerWidth > 720 ? window.innerWidth > 768 ? window.innerWidth > 1024 ? window.innerWidth > 1680 ? "xxlg" : "xlg" : "lg" : "r" : "md" : "sm" : "xs" : "xxs";
+        const w = window.innerWidth;
+        let result = [];
+        if (w > 320)
+            result.push("xxs");
+        if (w > 480)
+            result.push("xs");
+        if (w > 640)
+            result.push("sm");
+        if (w > 720)
+            result.push("md");
+        if (w > 768)
+            result.push("lg");
+        if (w > 1024)
+            result.push("xlg");
+        if (w > 1680)
+            result.push("xxlg");
+        return result;
+    },
+    screeHeight: () => {
+        const h = window.innerHeight;
+        let result = [];
+        if (h > 320)
+            result.push("xxs");
+        if (h > 480)
+            result.push("xs");
+        if (h > 640)
+            result.push("sm");
+        if (h > 720)
+            result.push("md");
+        if (h > 768)
+            result.push("lg");
+        if (h > 1024)
+            result.push("xlg");
+        if (h > 1680)
+            result.push("xxlg");
+        return result;
     }
 };
