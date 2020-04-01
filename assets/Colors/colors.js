af902664977c5fcebcabfeb3ffe9664f0b79694e
PassionPenguin <penguin.zhang@qq.com>
2020-04-01 22:39:43 +0800

    remove colors

---
+++ assets/Colors/colors.js
@@ -0,0 +1,247 @@
+/*
+
+    Package PassionPenguin/RailMapMaker
+
+
+    Created by @PassionPenguin
+    Last upd by @PassionPenguin
+
+ */
+
+/*
+* @ColorsComponents
+* Array
+*   => Object
+*       => name: Palette's name
+*       => color: Array
+*           => [0], Hex: String
+*           => [1], Name: String
+* */
+
+const colorPalette = [
+    {
+        // REF: Guangzhou MTR Official Database
+        name: "Guangzhou",
+        colors: [
+            [
+                "#F3D03E",
+                "Line 1"
+            ],
+            [
+                "#00629B",
+                "Line 2"
+            ],
+            [
+                "#ECA154",
+                "Line 3"
+            ],
+            [
+                "#00843D",
+                "Line 4"
+            ],
+            [
+                "#C5003E",
+                "Line 5"
+            ],
+            [
+                "#80225F",
+                "Line 6"
+            ],
+            [
+                "#97D700",
+                "Line 7"
+            ],
+            [
+                "#008C95",
+                "Line 8"
+            ],
+            [
+                "#71CC98",
+                "Line 9"
+            ],
+            [
+                "#7D9BC1",
+                "Line 10"
+            ],
+            [
+                "#470A68",
+                "Line 11"
+            ],
+            [
+                "#59621D",
+                "Line 12"
+            ],
+            [
+                "#8E8C13",
+                "Line 13"
+            ],
+            [
+                "#81312F",
+                "Line 14"
+            ],
+            [
+                "#AE8A79",
+                "Line 15"
+            ],
+            [
+                "#9E652E",
+                "Line 16"
+            ],
+            [
+                "#8B84D7",
+                "Line 17"
+            ],
+            [
+                "#D48BC8",
+                "Line 18"
+            ],
+            [
+                "#BB29BB",
+                "Line 19"
+            ],
+            [
+                "#D9027D",
+                "Line 20"
+            ],
+            [
+                "#201747",
+                "Line 21"
+            ],
+            [
+                "#041E42",
+                "Line 22"
+            ],
+            [
+                "#C4D600",
+                "Guangfo Line"
+            ],
+            [
+                "#00B5E2",
+                "APM Line"
+            ],
+            [
+                "#44A729",
+                "THZ1"
+            ],
+            [
+                "#AD96DC",
+                "Guangzhou-Foshan Loop Line"
+            ],
+            [
+                "#A45A2A",
+                "Guangzhou-Foshan-Zhaoqing Line"
+            ]
+        ]
+    },
+    {
+        // REF: Wikipedia, https://zh.wikipedia.org/wiki/%E4%BD%9B%E5%B1%B1%E5%9C%B0%E9%93%81
+        name: "Foshan",
+        colors: [
+            [
+                "#C4D600",
+                "Guangfo Line (Line 1)"
+            ],
+            [
+                "#009900",
+                "Line 2"
+            ],
+            [
+                "#0000FF",
+                "Line 3"
+            ],
+            [
+                "#FF00FF",
+                "Line 4"
+            ],
+            [
+                "#FFB81D",
+                "Line 6"
+            ],
+            [
+                "#A25EB5",
+                "Line 9"
+            ],
+            [
+                "#6600CC",
+                "Line 11"
+            ],
+            [
+                "#32B7EA",
+                "Line 13"
+            ]
+        ]
+    },
+    {
+        // REF: Wikipedia, https://zh.wikipedia.org/wiki/%E6%B7%B1%E5%9C%B3%E5%9C%B0%E9%93%81
+        name: "Shenzhen", colors: [
+            [
+                "#00B140",
+                "Line 1 (Luobao Line)"
+            ],
+            [
+                "#B94700",
+                "Line 2 (Shekou Line)"
+            ],
+            [
+                "#00A9E0",
+                "Line 3 (Longgang Line)"
+            ],
+            [
+                "#DA291C",
+                "Line 4 (Longhua Line)"
+            ],
+            [
+                "#A05EB5",
+                "Line 5 (Huanzhong Line)"
+            ],
+            [
+                "#00C7B1",
+                "Line 6"
+            ],
+            [
+                "#0033A0",
+                "Line 7"
+            ],
+            [
+                "#E45DBF",
+                "Line 8"
+            ],
+            [
+                "#7B6469",
+                "Line 9"
+            ],
+            [
+                "#F8779E",
+                "Line 10"
+            ],
+            [
+                "#672146",
+                "Line 11"
+            ],
+            [
+                "#A192B2",
+                "Line 12"
+            ],
+            [
+                "#DE7C00",
+                "Line 13"
+            ],
+            [
+                "#919D9D",
+                "Line 14"
+            ],
+            [
+                "#84BD00",
+                "Line 15"
+            ],
+            [
+                "#AEE2AE",
+                "Line 16"
+            ],
+            [
+                "#FFD700",
+                "Line 20"
+            ]
+        ]
+    }
+];
