af183db0c956a2a0852f7349a3296b69eabe6c8f
Chito Wong <9390786+wongchito@users.noreply.github.com>
2020-04-01 19:28:19 +0800

    clone the repo of RailMapGenerator => Original Repo Here: https://github.com/wongchito/RailMapGenerator/tree/master/src/panels/colour-diag/colours
    authored by Chito Wong<9390786+wongchito@users.noreply.github.com>
    
    ===
    
    Working on changing into JSON

---
+++ assets/Colours/other.js
@@ -0,0 +1,13 @@
+const Other: LineEntry[] = [
+    {
+        id: 'other',
+        name: {
+            en: 'Customise',
+            'zh-Hans': '自定义',
+            'zh-Hant': '自訂',
+        },
+        colour: '#aaaaaa',
+    },
+];
+
+export default Other;
