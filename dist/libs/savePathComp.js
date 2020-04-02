13813c7ba705a617de82737bb8e4ef451dcd1747
PassionPenguin <penguin.zhang@qq.com>
2020-04-02 17:09:06 +0800

    update time while save

--- dist/libs/savePathComp.js
+++ dist/libs/savePathComp.js
@@ -10,8 +10,10 @@
 
 const savePathComp = {
     current: () => {
+        contentData.lastModified = new Date().getTime();
         LocaleStorageManager.set("fileData_" + state.fileId, JSON.stringify(contentData));
     }, id: (id, data) => {
+        data.lastModified = new Date().getTime();
         LocaleStorageManager.set("fileData_" + id, JSON.stringify(data));
     }
 }
