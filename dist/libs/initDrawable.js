c06f33e27343ebf135717d95b54ac0b86fb93fab
PassionPenguin <penguin.zhang@qq.com>
2020-04-02 17:08:45 +0800

    support name

--- dist/libs/initDrawable.js
+++ dist/libs/initDrawable.js
@@ -12,7 +12,7 @@ const initDrawable = () => {
     contentData.pathInfo.forEach(e => {
         let svg = pg.$("#resSvg")[0];
 
-        let pathsGroup, stationsGroup;
+        let pathsGroup, stationsGroup, namesGroup;
         if (pg.$("#AllPathsGroup").length === 0) {
             pathsGroup = document.createElementNS(svg.namespaceURI, "g")
             pathsGroup.setAttributeNS(null, "id", "AllPathsGroup");
@@ -25,15 +25,30 @@ const initDrawable = () => {
             svg.appendChild(stationsGroup);
         } else stationsGroup = pg.$("#AllStationsGroup")[0];
 
+        if (pg.$("#stationNames").length === 0) {
+            namesGroup = document.createElementNS(svg.namespaceURI, "g")
+            namesGroup.setAttributeNS(null, "id", "stationNames");
+            svg.appendChild(namesGroup);
+        } else namesGroup = pg.$("#stationNames")[0];
+
+        namesGroup = document.createElementNS(svg.namespaceURI, "g");
+
         let path = document.createElementNS(svg.namespaceURI, "path");
         let stations = document.createElementNS(svg.namespaceURI, "g");
         path.setAttributeNS(null, "stroke", "#000");
-        path.setAttributeNS(null, "id", "UnnamedPath_" + e.id);
+        path.setAttributeNS(null, "id", "Path_" + e.id);
         path.setAttributeNS(null, "class", "pathElement");
-        stations.setAttributeNS(null, "id", "UnnamedStations_" + e.id);
+        stations.setAttributeNS(null, "id", "Stations_" + e.id);
         stations.setAttributeNS(null, "class", "stationsGroup");
         pathsGroup.appendChild(path);
         stationsGroup.appendChild(stations);
+
+        let names = document.createElementNS(svg.namespaceURI, "g");
+        names.setAttributeNS(null, "id", "Names_" + e.id);
+        names.setAttributeNS(null, "class", "nameGroup");
+        namesGroup.appendChild(names);
+        svg.appendChild(namesGroup);
+
     });
     for (let i = 0; i < contentData.pathInfo.length; i++) {
         drawMap(i);
